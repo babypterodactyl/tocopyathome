92a00517e672609823f27fdafa4d76356948d616
Sasha <sasha@babypterodactyl.com>
2017-05-30 16:40:55 -0500

    Add files via upload

---
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -0,0 +1,62 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+*/
+
+var bio = {
+  name: "Baby Pterodactyl",
+  role: "Front End Developer",
+
+  contacts: {
+    mobile: "210-840-7607",
+    email: "sasha@babypterodactyl.com",
+    github: "https://github.com/babypterodactyl",
+    location: "Austin"
+  },
+
+  welcomeMessage: "Hello, welcome to my page.",
+
+  skills: [
+    "HTML", "CSS", "JavaScript", "JQuery"
+  ],
+  bioPic: "https://cdn.pterodactyl.io/site-assets/logo-icon.png"
+};
+
+var education = {
+  schools: [{
+      name = "Udacity Nanodegree",
+      years = "2017 - 2017",
+      city = "Austin, TX",
+    },
+
+    {
+      name = "Viking Education Prep",
+      years = "2017 - 2017";
+      city = "Austin, TX"
+    }
+  ],
+
+  onlineCourses = [{
+    title: "Git and Github",
+    school: "Viking Code School",
+    dates: "2017",
+    url: "https://www.vikingcodeschool.com/prep"
+  }]
+
+};
+
+var work = {
+  jobs: [
+    {
+      employer: "Dynamic Systems",
+      title: "Administrative Assistant - Billing",
+      dates: "2017 - 2017",
+      description: "Provide administrative and billing support to the projects team"
+    },
+    {
+      employer: "University of Texas at Austin",
+      title: "Procurement Specialist",
+      dates: "2017 - 2017",
+      description: "Departmental buyer for UT School of Social Work"
+    }
+  ]
+};
